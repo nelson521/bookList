d874ac80d9bd9a7998ee3286ea0c24d6b495c664
Stevenson Nelson <sonson521@gmail.com>
2020-11-04 23:06:11 -0500

    add book and ui constructor. Also add event listener.

---
+++ app.js
@@ -0,0 +1,20 @@
+// Book Constructor
+function Book(title, author, isbn) {
+  this.title = title;
+  this.author = author;
+  this.isbn = isbn;
+}
+
+// UI Constructor
+function UI() {}
+
+// Event Listeners
+document.getElementById('book-form').addEventListener('submit', function (e) {
+  // Get forms values
+  const title = document.getElementById('title').value,
+    author = document.getElementById('author').value,
+    isbn = document.getElementById('isbn').value;
+
+  console.log(title, author, isbn);
+  e.preventDefault();
+});
